377fdaa49a631fb20911974c4e99c2d0d744f3da
Armanur Rashid <58313733+Arman13-arch@users.noreply.github.com>
2023-12-19 20:43:51 +0600

    home page banner

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,20 +1,40 @@
 // import React from 'react';
 import './mainpage.css'
+import hand_icon from '../../images/hand_icon.png'
+import arrow_icon from '../../images/arrow.png'
+import hero_image from '../../images/hero_image.png'
 
 const MainPage = () => {
     return (
-        // <div>
-        //     <h2>Home Page</h2>
-        // </div>
-        <div className='mainpage'>
-            <div className='img-div flex-item'>
-                <img src="/src/images/model2.png" alt="" />
+        <div className="hero">
+            <div className="hero-left">
+                <h2>NEW ARRIVALS ONLY</h2>
+                <div>
+                    <div className="hero-hand-icon">
+                        <p>New</p>
+                        <img src={hand_icon} alt="" />
+                    </div>
+                    <p>collections</p>
+                    <p>for everyone</p>
+                </div>
+                <div className="hero-latest-btn">
+                    <div>Latest Collection</div>
+                    <img src={arrow_icon} alt="" />
+                </div>
             </div>
-            <div className='text-div flex-item'>
-                <h1>New Collection <br/>For Fall</h1>
-                <h3>Discover all the new arrival of ready-to-wear collection. </h3>
+            <div className="hero-right">
+                <img className='hero_image' src={hero_image} alt="" />
             </div>
         </div>
+        // <div className='mainpage'>
+        //     <div className='img-div flex-item'>
+        //         <img src="/src/images/model2.png" alt="" />
+        //     </div>
+        //     <div className='text-div flex-item'>
+        //         <h1>New Collection <br/>For Fall</h1>
+        //         <h3>Discover all the new arrival of ready-to-wear collection. </h3>
+        //     </div>
+        // </div>
     );
 };
 
